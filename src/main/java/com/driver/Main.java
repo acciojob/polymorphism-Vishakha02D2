acab54c111de7499bee46e52574eb54e31931ace
Vishakha02D2 <dandagevishakha@gmail.com>
2023-01-12 23:53:20 +0530

    method overloading solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public void main(String args[]) {
+        Product p = new Product();
+        System.out.println(p.product(10, 20));
+        System.out.println(p.product(10, 20, 30));
+        System.out.println(p.product(10.5, 20.5));
+    }
+    class Product {
+        public int product(int x, int y) {
+            return (x * y);
+        }
+        public int product(int x, int y, int z) {
+            return (x * y * z);
+        }
+        public double product(double x, double y) {
+            return (x * y);
+        }
+    }
 }
